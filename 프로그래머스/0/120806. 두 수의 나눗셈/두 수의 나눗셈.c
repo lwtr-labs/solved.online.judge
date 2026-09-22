3159e05b07e702d7a2c640820f1fd5093863538f
lwtr-labs <lwtr.labs@gmail.com>
2026-09-22 15:17:53 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 5.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    
+    double x = ((double)num1/(double)num2)*1000;
+    
+    int answer = x;
+    return answer;
+}
